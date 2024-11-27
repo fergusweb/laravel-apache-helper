533e38e92b4ff9aebfc259f1a0223aa3fc6125e8
Anthony Ferguson <anthony@fergusweb.net>
2024-11-27 22:37:58 +1000

    Modify the lookup tool, so the input disappears, and a button/link to reload appears instead.  Livewire isn't updating for second input, requiring a reload, so this makes that easier.  Will fix the livewire issue another time.

--- resources/views/livewire/lookup-tool.blade.php
+++ resources/views/livewire/lookup-tool.blade.php
@@ -1,9 +1,14 @@
 <div>
     <form>
         <fieldset>
-            <legend>Paste list of IPs or rows from apache-status</legend>
 
-            <textarea id="inputText" wire:model.change="inputText" rows="6" spellCheck="false" placeholder="Paste data here..."></textarea>
+            @if (empty($inputText))
+                <legend>Paste list of IPs or rows from apache-status</legend>
+
+                <textarea id="inputText" wire:model.change="inputText" rows="6" spellCheck="false" placeholder="Paste data here..."></textarea>
+            @else
+                <a class="button" href="{{ url('/lookup') }}">Reload</a>
+            @endif
 
             @error('inputText')
                 <p class="error">{{ $message }}</p>
